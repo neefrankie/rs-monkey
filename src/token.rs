6011fad21216731c204a755548942688ecf9e88b
Victor <neefrankie@gamil.com>
2026-02-21 20:40:42 +0800

    ``` feat(token): 扩展token类型定义并添加关键字识别功能
    新增多种运算符token类型包括比较运算符和逻辑运算符，
    添加布尔值、条件判断和返回语句等关键字token，
    实现lookup_ident函数用于关键字识别和匹配
    ```

--- src/token.rs
+++ src/token.rs
@@ -2,21 +2,58 @@
 pub enum TokenType {
     Illegal,
     Eof,
+
+    // 标识符+字面量
     Ident,
     Int,
-    Assign,
-    Plus,
+
+    // 运算符
+    Assign, // =
+    Plus, // +
+    Minus, // -
+    Bang, // !
+    Asterisk, // *
+    Slash, // /
+
+    LessThan, // <
+    GreaterThan, // >
+
+    Eq, // ==
+    NotEq, // !=
+
+    // 分隔符
     Comma,
     Semicolon,
+
     LParen,
     RParen,
     LBrace,
     RBrace,
+
+    // 关键字
     Function,
     Let,
+    True,
+    False,
+    If,
+    Else,
+    Return,
 }
 
 pub struct Token {
     pub token_type: TokenType,
     pub literal: String,
+}
+
+pub fn lookup_ident(ident: &str) -> TokenType {
+    match ident {
+        "fn" => TokenType::Function,
+        "let" => TokenType::Let,
+        "true" => TokenType::True,
+        "false" => TokenType::False,
+        "if" => TokenType::If,
+        "else" => TokenType::Else,
+        "return" => TokenType::Return,
+        _ => TokenType::Ident,
+    }
 }
