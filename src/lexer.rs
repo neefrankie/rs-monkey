74467be2fb09c0ba0dc425d482bc2b4eaa296c89
Victor <neefrankie@gamil.com>
2026-02-21 20:40:54 +0800

    ``` feat(lexer): 实现词法分析器支持多种运算符和字面量解析
    添加了peek_char方法用于预览下一个字符，实现了对等号(==)、
    不等号(!=)、算术运算符(+,-,*,/)、比较运算符(<,>)的解析，
    增加了skip_whitespace方法跳过空白字符，并实现了数字
    字面量和标识符的解析功能。同时修复了测试用例中的预期
    token类型匹配问题。
    ```

--- src/lexer.rs
+++ src/lexer.rs
@@ -29,22 +29,64 @@ impl Lexer {
         self.read_position += 1;
     }
 
+    fn peek_char(&self) -> u8 {
+        if self.read_position >= self.input.len() {
+            0
+        } else {
+            self.input[self.read_position]
+        }
+    }
+
     pub fn next_token(&mut self) -> token::Token {
+        self.skip_whitespace();
+
         let tok = match self.ch {
-            b'=' => new_token(TokenType::Assign, self.ch),
+            b'=' => {
+                if self.peek_char() == b'=' {
+                    let ch = self.ch;
+                    self.read_char();
+                    let literal = char::from(ch).to_string() + &char::from(self.ch).to_string();
+                    token::Token{ token_type: TokenType::Eq, literal }
+                } else {
+                    new_token(TokenType::Assign, self.ch)
+                }
+            },
+            b'+' => new_token(TokenType::Plus, self.ch),
+            b'-' => new_token(TokenType::Minus, self.ch),
+            b'!' => {
+                if self.peek_char() == b'=' {
+                    let ch = self.ch;
+                    self.read_char();
+                    let literal = char::from(ch).to_string() + &char::from(self.ch).to_string();
+                    token::Token{ token_type: TokenType::NotEq, literal }
+                } else {
+                    new_token(TokenType::Bang, self.ch)
+                }
+            },
+            b'/' => new_token(TokenType::Slash, self.ch),
+            b'*' => new_token(TokenType::Asterisk, self.ch),
+            b'<' => new_token(TokenType::LessThan, self.ch),
+            b'>' => new_token(TokenType::GreaterThan, self.ch),
             b';' => new_token(TokenType::Semicolon, self.ch),
+            b',' => new_token(TokenType::Comma, self.ch),
             b'(' => new_token(TokenType::LParen, self.ch),
             b')' => new_token(TokenType::RParen, self.ch),
-            b',' => new_token(TokenType::Comma, self.ch),
-            b'+' => new_token(TokenType::Plus, self.ch),
             b'{' => new_token(TokenType::LBrace, self.ch),
             b'}' => new_token(TokenType::RBrace, self.ch),
             0 => token::Token { token_type: TokenType::Eof, literal: String::new() },
             _ => {
                 if is_letter(self.ch) {
                     let literal = self.read_identifier();
+                    let token_type = token::lookup_ident(&literal);
+                    // 提前退出
+                    return token::Token { token_type, literal };
+                } if is_digit(self.ch) {
+                    let literal = self.read_number();
+                    return token::Token { token_type: TokenType::Int, literal };
+                } else {
+                    new_token(TokenType::Illegal, self.ch)
                 }
-                new_token(TokenType::Illegal, self.ch)
+                
             },
         };
         self.read_char();
@@ -58,6 +100,22 @@ impl Lexer {
         }
         String::from_utf8_lossy(&self.input[position..self.position]).to_string()
     }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch == b' ' || self.ch == b'\t' || self.ch == b'\n' || self.ch == b'\r' {
+            self.read_char();
+        }
+    }
+
+    fn read_number(&mut self) -> String {
+        let position = self.position;
+        while is_digit(self.ch) {
+            self.read_char();
+        }
+        String::from_utf8_lossy(&self.input[position..self.position]).to_string()
+    }
+
+    
 }
 
 fn new_token(kind: token::TokenType, ch: u8) -> token::Token {
@@ -68,7 +126,11 @@ fn new_token(kind: token::TokenType, ch: u8) -> token::Token {
 }
 
 fn is_letter(ch: u8) -> bool {
-    ch.is_ascii_alphanumeric() || ch == b'_'
+    ch.is_ascii_alphabetic() || ch == b'_'
+}
+
+fn is_digit(ch: u8) -> bool {
+    ch.is_ascii_digit()
 }
 
 
@@ -86,8 +148,8 @@ let add = fn(x, y) {
 };
 
 let result = add(five, ten);
-!-/*5
-5 < 10 > 5
+!-/*5;
+5 < 10 > 5;
 
 if (5 < 10) {
 	return true;
@@ -97,10 +159,7 @@ if (5 < 10) {
 
 10 == 10;
 10 != 9;
-"foobar"
-"foo bar"
-[1, 2];
-{"foo": "bar"}"#;
+"#;
 
         let tests = vec![
             (TokenType::Let, "let"),
@@ -115,7 +174,7 @@ if (5 < 10) {
             (TokenType::Semicolon, ";"),
             (TokenType::Let, "let"),
             (TokenType::Ident, "add"),
-            (TokenType::Ident, "="),
+            (TokenType::Assign, "="),
             (TokenType::Function, "fn"),
             (TokenType::LParen, "("),
             (TokenType::Ident, "x"),
@@ -131,13 +190,50 @@ if (5 < 10) {
             (TokenType::Semicolon, ";"),
             (TokenType::Let, "let"),
             (TokenType::Ident, "result"),
-            (TokenType::Ident, "="),
+            (TokenType::Assign, "="),
             (TokenType::Ident, "add"),
             (TokenType::LParen, "("),
             (TokenType::Ident, "five"),
             (TokenType::Comma, ","),
             (TokenType::Ident, "ten"),
             (TokenType::RParen, ")"),
+            (TokenType::Semicolon, ";"),
+            (TokenType::Bang, "!"),
+            (TokenType::Minus, "-"),
+            (TokenType::Slash, "/"),
+            (TokenType::Asterisk, "*"),
+            (TokenType::Int, "5"),
+            (TokenType::Semicolon, ";"),
+            (TokenType::Int, "5"),
+            (TokenType::LessThan, "<"),
+            (TokenType::Int, "10"),
+            (TokenType::GreaterThan, ">"),
+            (TokenType::Int, "5"),
+            (TokenType::Semicolon, ";"),
+            (TokenType::If, "if"),
+            (TokenType::LParen, "("),
+            (TokenType::Int, "5"),
+            (TokenType::LessThan, "<"),
+            (TokenType::Int, "10"),
+            (TokenType::RParen, ")"),
+            (TokenType::LBrace, "{"),
+            (TokenType::Return, "return"),
+            (TokenType::True, "true"),
+            (TokenType::Semicolon, ";"),
+            (TokenType::RBrace, "}"),
+            (TokenType::Else, "else"),
+            (TokenType::LBrace, "{"),
+            (TokenType::Return, "return"),
+            (TokenType::False, "false"),
+            (TokenType::Semicolon, ";"),
+            (TokenType::RBrace, "}"),
+            (TokenType::Int, "10"),
+            (TokenType::Eq, "=="),
+            (TokenType::Int, "10"),
+            (TokenType::Semicolon, ";"),
+            (TokenType::Int, "10"),
+            (TokenType::NotEq, "!="),
+            (TokenType::Int, "9"),
             (TokenType::Semicolon, ";"),
             (TokenType::Eof, ""),
         ];
