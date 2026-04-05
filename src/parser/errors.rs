31928f3d09af23d0c6e28176bf6f470f451a9293
Victor <neefrankie@gamil.com>
2026-04-05 22:24:52 +0800

    feat(parser): 为ParseError实现Display和Error trait

--- src/parser/errors.rs
+++ src/parser/errors.rs
@@ -1,3 +1,5 @@
+use std::{error, fmt};
+
 use crate::token::TokenType;
 
 
@@ -22,8 +24,39 @@ pub enum ParseError {
 }
 
 
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ParseError::UnexpectedToken { expected, got } => {
+                write!(
+                    f,
+                    "Unexpected token. Expected {}, got {}",
+                    expected, got
+                )
+            }
+            ParseError::InvalidSyntax { message } => {
+                write!(f, "Invalid syntax: {}", message)
+            }
+            ParseError::ExpectedInt { got } => {
+                write!(f, "Cound not parse {} as integer", got)
+            }
+            ParseError::NoPrefixParseFn { token_type } => {
+                write!(f, "No prefix parse function for {}", token_type)
+            }
+            ParseError::NoInfixParseFn { token_type } => {
+                write!(f, "No infix parse function for {}", token_type)
+            }
+        }
+    }
+
+}
+
 // #[derive(Debug, Clone, PartialEq)]
 // pub struct Position {
 //     pub line: usize,
 //     pub column: usize,
 // }
+
+impl error::Error for ParseError {
+    
+}
