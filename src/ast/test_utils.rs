036bae840145111c407200a7437b7e33960378e0
Victor <neefrankie@gamil.com>
2026-04-09 18:14:44 +0800

    feat(ast): 添加测试工具函数支持多种表达式类型

--- src/ast/test_utils.rs
+++ src/ast/test_utils.rs
@@ -62,7 +62,68 @@ pub fn new_infix_expr(left: Expression, operator: &str, right: Expression) -> Ex
     }
 }
 
+pub fn new_if_expr(
+    condition: Expression, 
+    consequence: BlockStatement,
+    alternative: Option<BlockStatement>
+) -> Expression {
+    Expression::If {
+        token: token_from_str("if"),
+        condition: Rc::new(condition),
+        consequence: Rc::new(consequence),
+        alternative: if let Some(block) = alternative {
+            Some(Rc::new(block))
+        } else {
+            None
+        },
+    }
+}
 
+pub fn new_func_expr(
+    params: Vec<&str>, 
+    body: BlockStatement
+) -> Expression {
+    Expression::FunctionLiteral {
+        token: token_from_str("fn"),
+        parameters: params.iter().map(
+            |s| new_identifier(s)
+        ).collect(),
+        body: Rc::new(body),
+    }
+}
+
+pub fn new_call_expr(
+    function: Expression,
+    args: Vec<Expression>
+) -> Expression {
+    Expression::Call {
+        token: token_from_str("("),
+        function: Rc::new(function),
+        arguments: args,
+    }
+}
+
+pub fn new_array_expr(elements: Vec<Expression>) -> Expression {
+    Expression::ArrayLiteral {
+        token: token_from_str("["),
+        elements: elements,
+    }
+}
+
+pub fn new_hash_expr(pairs: Vec<(Expression, Expression)>) -> Expression {
+    Expression::HashLiteral {
+        token: token_from_str("{"),
+        pairs: pairs,
+    }
+}
+
+pub fn new_index_expr(left: Expression, right: Expression) -> Expression {
+    return Expression::Index {
+        token: token_from_str("["),
+        left: Rc::new(left),
+        index: Rc::new(right),
+    };
+}
 
 pub fn new_let_stmt(name: &str, value: Expression) -> Statement {
     Statement::Let {
